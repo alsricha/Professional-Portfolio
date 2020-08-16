fba904ba0afd51f19743273611682abb56cb9272
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-08-16 16:38:52 -0600

    Media Queries

--- Assets/scripts/main.js
+++ Assets/scripts/main.js
@@ -28,3 +28,40 @@ function linkAction(){
 }
 
 navLink.forEach(ntimes => ntimes.addEventListener('click', linkAction))
+
+/* --- SCROLL ANIMATION --- */
+const sr =ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+
+})
+
+/* SCROLL HOME SECTION */
+
+sr.reveal('.home-title', {})
+sr.reveal('.button', {delay: 200})
+sr.reveal('.home_img', {delay: 400})
+sr.reveal('.home_social-icon', {interval: 200})
+
+/* SCROLL ABOUT SECTION */
+
+sr.reveal('.about_img', {})
+sr.reveal('.about-subtitle', {delay: 200})
+sr.reveal('.about-text', {delay: 400})
+
+/* SCROLL SKILLS SECTION */
+
+sr.reveal('.skills_subtitle', {})
+sr.reveal('.skills_text', {delay: 200})
+sr.reveal('.skills_info', {interval: 200})
+sr.reveal('.skills_img', {delay: 400})
+
+/* SCROLL PORTFOLIO SECTIPN */
+
+sr.reveal('.portfolio_img', {interval: 200})
+
+/*SCROLL CONTACT SECTION  */
+
+sr.reveal('.contact_input', {interval: 200})
